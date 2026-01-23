11589ec106b0aa94cd119a6a5ab1d2680f8cb0fc
omnialnalinila <ktonhuang28@gmail.com>
2026-01-23 11:26:38 -1000

    made progress on the account creation page (i guess im leaning more to an account creation page more than a login page) text inputs are present; still trying to work checking the text inputs to see if they're empty or null before going forward

--- app/login.js
+++ app/login.js
@@ -1,5 +1,60 @@
-import { Text, TextInput, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView, Alert } from 'react-native';
+import { useState } from 'react'
 
-export default function logIn(){
-    
+export default function App() {
+  const [username, onChangeUsername] = useState()
+  const [password, onChangePassword] = useState()
+  function checkDetails(){
+    if(username != null && username != ""){
+        console.log("Username is present!")
+    }
+
+    if(username == null || username == ""){
+      console.warn("username is blank")
+      Alert.alert('USERNAME WARNING', 'PLEASE GIVE AN ACTUAL USERNAME.')
+    } else {
+      console.log("username (" + username + ") is not blank")
+    }
+    if(password == null || password == ""){
+      console.warn("password is blank")
+      Alert.alert('PASSWORD WARNING', 'PLEASE GIVE AN ACTUAL PASSWORD.')
+    } else {
+      console.log("password (" + password + ") is not blank")
+    }
+  }
+
+  return (
+    <SafeAreaView>
+    <TextInput
+      placeholder="Put In Your Desired Username"
+      onChangeText={onChangeUsername}
+      value={username}
+      style={styles.Haruka}
+      maxLength={40}
+    />
+    <TextInput
+      placeholder="Put In Your Desired Password"
+      onChangeText={onChangePassword}
+      value={password}
+      style={styles.Haruka}
+      maxLength={40}
+    />
+    <Button
+      title="PRESS ME"
+      onPress={checkDetails}
+    />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  Haruka: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  Hoshiguma: {
+
+  }
+});
