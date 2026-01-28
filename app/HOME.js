32e2ff1c95c532fba93dc3c1cbe0a1cfd187fe31
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-01-28 13:18:02 -0800

    testing!

---
+++ app/HOME.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+export default function App() {
+
+    return (
+        <View>
+            <Text>TESTING 123</Text>
+        </View>
+
+
+
+    ); 
+}
