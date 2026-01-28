710d00e95c300e842c07ea12919313a939414b9e
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-01-28 14:06:05 -0800

    a

--- app/HOME.js
+++ app/HOME.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
+import {Link} from "export-router";
+import LinkButton from "../components/linkButton";
 
 export default function App() {
 
     return (
         <View>
             <Text>TESTING 123</Text>
+            <LinkButton page="/(tABS)/homeScreen" title="tests"/>
         </View>
 
 
