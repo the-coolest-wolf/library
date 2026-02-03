dca746e6bbcbbed4045d34296de01355bb7bb378
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-02-03 15:22:23 -0800

    has linkbutton

---
+++ app/HOME.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import LinkButton from "../components/linkButton";
+
+export default function App() {
+
+    return (
+        <View>
+            <Text>TESTING 123</Text>
+            <LinkButton page="/(tABS)/homeScreen" title="tests"/>
+        </View>
+
+
+
+    ); 
+}
