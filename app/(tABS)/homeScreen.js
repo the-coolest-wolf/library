c79c481363e1ef7f4a9522f60fc2f38fa991f3c7
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-02-04 13:18:26 -0800

    a

---
+++ app/(tABS)/homeScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+const lemonadeStand = () =>(
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.awesomeScrollyView}>
+        <Text>Testing 123</Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+export default function App(){
+  return(
+    lemonadeStand()
+  );
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },awesomeScrollyView:{
+    backgroundColor:'#adeeea'
+  },
+});
