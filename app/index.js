eef0769a23cbd917e369950336803a14b54f10b6
omnialnalinila <ktonhuang28@gmail.com>
2026-02-12 11:48:46 -1000

    PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE PLEASE

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import { View, Text } from "react-native";
 import LinkButton from "../components/LinkButton";
+import { Databases, Client } from 'appwrite'
+
+export const databases = new Databases(Client)
+
 export default function tester(){
     return (
         <View>
