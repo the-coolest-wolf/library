8cf7ee0a6db0de02c7a0788d9e28bd2c728e2b35
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-02-27 12:30:04 -0800

    finished with the basics of public screen

--- app/publicScreen.js
+++ app/publicScreen.js
@@ -10,17 +10,17 @@ export default function App(){
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
               <ScrollView style={styles.awesomeScrollyView}>
-            <Text>123</Text>
-            <Text>Name</Text>
-            <Text>Username</Text>
-            <Image source={require("../assets/battleGround.jpg")}/>
-            <Text>Favorite Book</Text>
-            <Image source={require("../assets/author1.jpg")}/>
-            <Text>Favorite Author</Text>
-            <Image source={require("../assets/battleGround.jpg")}/>
-            <Text>Reccomendation for New Readers</Text>
+                <Text style={styles.textz}>CLASSIFIED</Text>
+                <Text style={styles.textz}>the_wolf</Text>
+                <Link style={styles.linkStyle1} href="../homeScreen">Back</Link>
+                <Image style={styles.book} source={require("../assets/sunlitMan.webp")}/>
+                <Text style={styles.texts}>Favorite Book</Text>
+                <Image style={styles.authorStyle} source={require("../assets/author1.jpg")}/>
+                <Text style={styles.texts}>Favorite Author</Text>
+                <Image style={styles.book} source={require("../assets/Zen_motorcycle.jpg")}/>
+                <Text style={styles.texts}>Reccomendation for New Readers</Text>
 
-        </ScrollView>
+              </ScrollView>
             </SafeAreaView>
           </SafeAreaProvider>
     );
@@ -28,4 +28,10 @@ export default function App(){
 const styles = StyleSheet.create({
   container:{flex: 1,paddingTop: StatusBar.currentHeight,},
   awesomeScrollyView:{backgroundColor:'#97c19b'},
+  book:{height:500,width:279.5,marginTop: 40,marginLeft:40,borderColor:'black',borderWidth:5,marginBottom:-75,},
+  authorStyle:{height:270,width:270,marginLeft:40,marginTop:75,borderColor:'black',borderWidth:5,},
+  texts:{top:-230,fontWeight:20, alignSelf:'flex-end',marginRight:40,fontWeight:100,fontSize:50},
+  linkStyle3:{top:-300,marginLeft:100,alignSelf:'flex-end'},
+  textz:{fontWeight:10000,alignSelf:'center', fontSize:60,},
+  linkStyle1:{fontSize:40, top:-150, marginLeft:10, borderColor:'black', borderWidth:5,width:95,backgroundColor:'white',},
 });
