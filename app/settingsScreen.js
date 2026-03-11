e27af55076cbbdce66c257c3fa17b6c9deac8cf7
the-coolest-wolf <hunterhignight.tx@gmail.com>
2026-03-11 11:46:58 -0700

    not done but presenting now so pushing

--- app/settingsScreen.js
+++ app/settingsScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 export default function tester(){
     return (
         <View syle={styles.container}>
-            
+            <Text>Background</Text>
         </View>
     );
 }
