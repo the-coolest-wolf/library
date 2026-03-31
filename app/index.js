01702f7d211c12149507deac38316c12c68598a8
omnialnalinila <ktonhuang28@gmail.com>
2026-03-31 10:39:20 -1000

    im sus

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import { View, Text } from "react-native";
 import LinkButton from "../components/LinkButton";
-//import { databases, Client } from 'appwrite'
+import databases from 'appwrite'
+
+
 
 export default function App(){
     return (
         <View>
         <Text>good day!</Text>
-        <LinkButton page="homeScreen" title="home page"/>
-        <LinkButton page="login" title="login page"/>
-        <LinkButton page="hunterSearch" title="search page"/> 
+        <Text></Text>
+            <LinkButton page="homeScreen" title="home page"/>
+            <LinkButton page="login" title="login page"/>
+            <LinkButton page="hunterSearch" title="search page"/> 
         </View>
     )
 }
